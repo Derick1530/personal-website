e6d83010b51f2ddacd482bcc69af93c087031158
Shadow Smith <hey@shadowsmith.com>
2022-02-01 23:13:42 -0600

    Update buttons

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -10,11 +10,11 @@
       <audio v-if="content.track" controls class="mb-6">
         <source :src="content.track" type="audio/mpeg">
       </audio>
-      <div v-if="content.buttonLink && content.buttonText || content.body.children.length > 0" class="mt-6">
+      <div v-if="(content.buttonText && content.externalLink) || (content.buttonText && content.internalLink)" class="mt-6">
         <a
-          v-if="content.buttonLink && content.buttonText"
-          :href="content.buttonLink"
-          class="inline-flex items-center bg-black text-white border border-black rounded py-2 px-12 mr-2"
+          v-if="content.buttonText && content.externalLink"
+          :href="content.externalLink"
+          class="inline-flex items-center bg-black text-white border border-black rounded py-2 px-12"
           target="_blank"
           rel="noopener noreferrer"
         >{{ content.buttonText }} <svg
@@ -29,15 +29,13 @@
           stroke-linejoin="round"
           class="feather feather-external-link ml-2"
         ><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" /><polyline points="15 3 21 3 21 9" /><line x1="10" y1="14" x2="21" y2="3" /></svg></a>
-        <button v-if="content.body.children.length > 0" class="bg-white text-gray-500 border rounded py-2 px-12" @click="showContent = !showContent">
-          <template v-if="!showContent">
-            Read
-          </template>
-          <template v-else>
-            Hide
-          </template>
-          <span v-if="content.track">Lyrics</span>
-        </button>
+        <nuxt-link
+          v-if="content.buttonText && content.internalLink"
+          :to="content.internalLink"
+          class="inline-flex items-center bg-black text-white border border-black rounded py-2 px-12"
+        >
+          {{ content.buttonText }}
+        </nuxt-link>
       </div>
     </div>
     <div class="flex items-center my-8">
@@ -51,7 +49,7 @@
         </div>
       </div>
     </div>
-    <div v-if="showContent">
+    <div v-if="content.body.children.length > 0">
       <nuxt-content :document="content" />
       <Footer />
     </div>
