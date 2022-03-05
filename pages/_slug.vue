6afe53c44d4145cee103b2c9c202a6ff32df0696
Shadow Smith <hey@shadowsmith.com>
2022-03-05 15:54:55 -0600

    Remove url parameter

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -80,7 +80,7 @@ export default {
         {
           hid: 'og:image',
           property: 'og:image',
-          content: `/api/social-image?title=${this.content.title}&type=${this.content.type}`
+          content: `/api/social-image?title=${this.content.title}`
         }
       ]
     }
